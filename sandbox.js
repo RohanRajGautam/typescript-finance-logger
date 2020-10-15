e2dfc0b8caa5bb8dad4722c447aee3113185f57d
rohan <rohanrajgautam@gmail.com>
2020-10-15 18:27:10 +0545

    compiling typesript

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var person = 'Rohan';
+console.log(person);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
+console.log('Compling');
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+const person = 'Rohan';
+
+console.log(person)
+
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(input => {
+    console.log(input)
+})
+
+console.log('Compling')
