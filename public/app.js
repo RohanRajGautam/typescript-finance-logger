ea02e1e0dd41dcdc064db98cc3309a26db11da04
rohan <rohanrajgautam@gmail.com>
2020-10-17 13:02:12 +0545

    added various classes

--- public/app.js
+++ public/app.js
@@ -1,6 +1,22 @@
 "use strict";
+var Invoice = /** @class */ (function () {
+    function Invoice(c, a, d) {
+        this.client = c;
+        this.amount = a;
+        this.details = d;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('rohit', 80, 'personal reason');
+var invTwo = new Invoice('badal', 100, 'personal reason');
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 var form = document.querySelector('.new-item-form');
-console.log(form.children);
 var type = document.querySelector('#type');
 var toFrom = document.querySelector('#tofrom');
 var details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,32 @@
-const form = document.querySelector('.new-item-form') as HTMLFormElement
+class Invoice{
+    client: string;
+    amount: number;
+    details: string;
 
-console.log(form.children);
+    constructor(c: string, a: number, d: string){
+        this.client = c;
+        this.amount = a;
+        this.details = d;
+    }
+
+    format(){
+        return `${this.client} owes $${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoice('rohit', 80, 'personal reason');
+const invTwo = new Invoice('badal', 100, 'personal reason');
+
+const invoices: Invoice[] = [];
+
+invoices.push(invOne)
+invoices.push(invTwo)
+
+console.log(invoices);
+
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement
 
 const type = document.querySelector('#type') as HTMLSelectElement
 const toFrom = document.querySelector('#tofrom') as HTMLInputElement
