b8cec44221a493df8afb7dfb3646a204fbd39f30
rohan <rohanrajgautam@gmail.com>
2020-10-17 12:11:33 +0545

    initial app setup

--- sandbox.js
+++
@@ -1,9 +0,0 @@
-var character = 'Rohan';
-var age = 21;
-var isAwesome = true;
-// character = 12
-// character = 'Gautam';
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(7));
