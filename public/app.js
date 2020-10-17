22e8973e3609235f247cc92d13e1943e37220cae
rohan <rohanrajgautam@gmail.com>
2020-10-17 12:47:20 +0545

    dom and typecasting

--- public/app.js
+++ public/app.js
@@ -1,1 +1,11 @@
 "use strict";
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+var type = document.querySelector('#type');
+var toFrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
